d7971f5d2d9d1c482aeb4da5a38cc4735910988b
d3167502 <Tian@Tians-MBP.hsd1.ca.comcast.net>
2018-05-06 22:51:35 -0700

    Final

--- CrimeMapping/client/dist/main.bundle.js
+++ CrimeMapping/client/dist/main.bundle.js
@@ -241,11 +241,225 @@ var AppComponent = /** @class */ (function () {
     // tslint:disable-next-line:use-life-cycle-interface
     AppComponent.prototype.ngOnInit = function () {
         var _this = this;
-        var styles = this.mapStyle;
+        var styles = [
+            {
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#ebe3cd'
+                    }
+                ]
+            },
+            {
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#523735'
+                    }
+                ]
+            },
+            {
+                'elementType': 'labels.text.stroke',
+                'stylers': [
+                    {
+                        'color': '#f5f1e6'
+                    }
+                ]
+            },
+            {
+                'featureType': 'administrative',
+                'elementType': 'geometry.stroke',
+                'stylers': [
+                    {
+                        'color': '#c9b2a6'
+                    }
+                ]
+            },
+            {
+                'featureType': 'administrative.land_parcel',
+                'elementType': 'geometry.stroke',
+                'stylers': [
+                    {
+                        'color': '#dcd2be'
+                    }
+                ]
+            },
+            {
+                'featureType': 'administrative.land_parcel',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#ae9e90'
+                    }
+                ]
+            },
+            {
+                'featureType': 'landscape.natural',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#dfd2ae'
+                    }
+                ]
+            },
+            {
+                'featureType': 'poi',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#dfd2ae'
+                    }
+                ]
+            },
+            {
+                'featureType': 'poi',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#93817c'
+                    }
+                ]
+            },
+            {
+                'featureType': 'poi.park',
+                'elementType': 'geometry.fill',
+                'stylers': [
+                    {
+                        'color': '#a5b076'
+                    }
+                ]
+            },
+            {
+                'featureType': 'poi.park',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#447530'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#f5f1e6'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.arterial',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#fdfcf8'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.highway',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#f8c967'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.highway',
+                'elementType': 'geometry.stroke',
+                'stylers': [
+                    {
+                        'color': '#e9bc62'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.highway.controlled_access',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#e98d58'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.highway.controlled_access',
+                'elementType': 'geometry.stroke',
+                'stylers': [
+                    {
+                        'color': '#db8555'
+                    }
+                ]
+            },
+            {
+                'featureType': 'road.local',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#806b63'
+                    }
+                ]
+            },
+            {
+                'featureType': 'transit.line',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#dfd2ae'
+                    }
+                ]
+            },
+            {
+                'featureType': 'transit.line',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#8f7d77'
+                    }
+                ]
+            },
+            {
+                'featureType': 'transit.line',
+                'elementType': 'labels.text.stroke',
+                'stylers': [
+                    {
+                        'color': '#ebe3cd'
+                    }
+                ]
+            },
+            {
+                'featureType': 'transit.station',
+                'elementType': 'geometry',
+                'stylers': [
+                    {
+                        'color': '#dfd2ae'
+                    }
+                ]
+            },
+            {
+                'featureType': 'water',
+                'elementType': 'geometry.fill',
+                'stylers': [
+                    {
+                        'color': '#b9d3c2'
+                    }
+                ]
+            },
+            {
+                'featureType': 'water',
+                'elementType': 'labels.text.fill',
+                'stylers': [
+                    {
+                        'color': '#92998d'
+                    }
+                ]
+            }
+        ];
         this.map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 37.51157, lng: -122.134 },
             zoom: 11,
-            styles: this.mapStyle,
+            styles: styles,
             mapTypeControl: false
         });
         this.map.addListener('click', function (e) {
@@ -646,7 +860,6 @@ var AppComponent = /** @class */ (function () {
                         this_2.markers[i].infowindow = infowindow_1;
                         google.maps.event.addListener(infowindow_1, 'domready', function () {
                             // now my elements are ready for dom manipulation
-                            console.log('eee' + i + '');
                             var clickableItem = document.getElementById('eee' + i + '');
                             clickableItem.addEventListener('click', function () {
                                 _this.displayDirections(origins[i]);
